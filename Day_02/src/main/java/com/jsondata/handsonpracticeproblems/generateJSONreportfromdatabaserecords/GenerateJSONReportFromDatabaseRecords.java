480e1668d10d8f933c554057043bbd4bbff34af3
suraj patel <sp9589890205@gmail.com>
2025-02-19 22:51:08 +0530

    [Suraj] added Generate a JSON report from database records.

---
+++ Day_02/src/main/java/com/jsondata/handsonpracticeproblems/generateJSONreportfromdatabaserecords/GenerateJSONReportFromDatabaseRecords.java
@@ -0,0 +1,30 @@
+package com.jsondata.handsonpracticeproblems.generateJSONreportfromdatabaserecords;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.*;
+
+public class GenerateJSONReportFromDatabaseRecords {
+    public static void main(String[] args) {
+        //Try block
+        try {
+            // Database records as a list
+            List<Student> students = Arrays.asList(
+                    new Student("Pratham", 25, "pratham@example.com"),
+                    new Student("Vivek", 22, "vivek@example.com"),
+                    new Student("Suraj", 28, "suraj@example.com")
+            );
+
+            // Converting list to JSON and write to file
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(new File("src/main/java/com/jsondata/handsonpracticeproblems/generateJSONreportfromdatabaserecords/students_report.json"), students);
+
+            System.out.println("JSON report generated successfully!");
+        }
+        //Catch block
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
