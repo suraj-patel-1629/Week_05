fafef5704599cc517ade77b84d0ee462bef289d8
suraj patel <sp9589890205@gmail.com>
2025-02-19 07:05:59 +0530

    [Suraj] added Advance-Problem  Detect Duplicates in a CSV File

---
+++ Day_01/src/main/java/com/csvdatahandling/advanceproblems/detectduplicatesinacsvfile/DetectDuplicatesInACSVFile.java
@@ -0,0 +1,12 @@
+package com.csvdatahandling.advanceproblems.detectduplicatesinacsvfile;
+
+//import static com.csvdatahandling.advancedproblems.detectduplicatesinacsvfile.DetectingDuplicatesInACSVFile.detectDuplicates;
+
+public class DetectDuplicatesInACSVFile {
+    public static void main(String[] args) {
+        //FilePath
+        String filePath = "src/main/java/com/csvdatahandling/advanceproblems/detectduplicatesinacsvfile/file.csv";
+        //Calling method
+        DetectingDuplicatesInACSVFile.detectDuplicates(filePath);
+    }
+}
